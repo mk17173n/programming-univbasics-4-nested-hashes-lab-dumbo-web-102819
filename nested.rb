2f1153e36011b1e11ead00c5edb713780a1c9e82
Monsur Khan <monsur.khan@gmail.com>
2019-10-23 19:05:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,7 +1,6 @@
-
 def hopper
-	programmer_hash = 
- 		{
+programmer_hash =
+  {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -16,14 +15,17 @@ def hopper
         }
      }
 
-
+    programmer_hash[:grace_hopper]
 end
+
+
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
-	programmer_hash = 
- 		{
+# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+
+
+programmer_hash =
+  {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -36,12 +38,16 @@ def alan_kay_is_known_for
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+     }#End programmer_hash
+     
+  programmer_hash[:alan_kay][:known_for]
 end
 
+
+
 def dennis_ritchies_language
-	programmer_hash = 
- 		{
+programmer_hash =
+  {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -55,7 +61,11 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:dennis_ritchie][:languages][0]  
 end
+
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -64,8 +74,13 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = 
- 		{
+programmer_hash =
+  {
+   
+        :yukihiro_matsumoto => {
+           :known_for => "Ruby",
+           :languages => ["LISP", "C"]
+        },    
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -78,14 +93,15 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
+       
      }
 
-    
+   
 end
 
 def changing_alan
-	programmer_hash = 
- 		{
+programmer_hash =
+  {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -99,15 +115,18 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
-     
+     return programmer_hash
 end
+
+
 
 def adding_to_dennis
-	programmer_hash = 
- 		{
+programmer_hash =
+  {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -121,5 +140,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+ 
+  programmer_hash
 end
+
